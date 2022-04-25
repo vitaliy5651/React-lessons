2627bfe8010328764a6e5290165e4a7c83efdccc
Vitaliy <vitaliy.kaliskarov@mail.ru>
2022-04-25 13:47:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let user = []
+  user.push({
+    name: 'Jhon',
+    age: 30,
+    job: 'Programer'
+  })
+  user.push({
+    name: 'Elena',
+    age: 20,
+    job: 'Administrator',
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {user[0].job + ' ' + user[1].job}
     </div>
   );
 }
