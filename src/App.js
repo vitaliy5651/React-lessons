b72e6d42803810306c46018aac38a7047b15342c
Vitaliy <vitaliy.kaliskarov@mail.ru>
2022-04-27 15:36:26 +0300

    15:35 27.04.2022

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import './App.css';
+import Buttons from './buttons';
+import btn from './Buttons.css'
 
 function App() {
-  let user = []
-  user.push({
-    name: 'Jhon',
-    age: 30,
-    job: 'Programer'
-  })
-  user.push({
-    name: 'Elena',
-    age: 20,
-    job: 'Administrator',
-  })
-  return (
-    <div className="App">
-    {user[0].job + ' ' + user[1].job}
-    </div>
-  );
+return(
+  <Buttons style = {btn}></Buttons>
+)
 }
 
 export default App;
---
+++ src/buttons.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Histories from './history';
+
+class Buttons extends React.Component{
+constructor(){
+    super()
+    this.state = {
+        id: []
+    }
+}
+
+handleClick = (value) => {
+    let historyClick = this.state.id
+    if(historyClick.length >= 3){
+        historyClick.shift()
+    }
+    historyClick.push(value)
+    this.setState({id: historyClick})
+}
+
+render(){
+    return(
+        <div className='wrapper'>
+        <div className='btn'>
+            <button onClick={() =>this.handleClick(1)}>1</button>
+            <button onClick={() =>this.handleClick(2)}>2</button>
+            <button onClick={() =>this.handleClick(3)}>3</button>
+            <button onClick={() =>this.handleClick(4)}>4</button>
+            <button onClick={() =>this.handleClick(5)}>5</button>
+        </div>
+        <Histories getValue = {this.state.id} ></Histories>
+        </div>
+    )
+}
+}
+
+export default Buttons
