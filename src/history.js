fbe2dfa5375b4cb55c4478937a7c05f17f8062e9
Vitaliy Kaliskarov <vitaliy.kaliskarov@mail.ru>
2022-05-01 11:16:12 +0300

    11:14 01.05.2022

--- src/history.js
+++ src/history.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 class Histories extends React.Component{
-    constructor(props){
-        super(props)
-    }
+
     render(){
         return(
             <div className='histories'>History of click: {this.props.getValue.join('')} </div>
